21f3c74d746d2a01676ebbc902a87ebdbe19be3f
mericozkayagan <mericozkayagan@gmail.com>
2022-05-16 16:13:57 +0300

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -3,6 +3,22 @@ resource "aws_security_group" "default" {
   name_prefix = "${var.name}-Client-VPN"
   description = "security group allowing egress for client-vpn users"
   vpc_id      = var.vpc_id
+  ingress {
+    description      = "Allow all traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
     Name               = "${var.name}-Client-VPN"
@@ -10,14 +26,4 @@ resource "aws_security_group" "default" {
     Service            = "client-vpn"
     TerraformWorkspace = terraform.workspace
   }
-}
-
-resource "aws_security_group_rule" "default_egress_world" {
-  count             = var.security_group_id == "" ? 1 : 0
-  type              = "egress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "all"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.default[0].id
 }
